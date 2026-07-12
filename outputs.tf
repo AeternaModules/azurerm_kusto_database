75fb490989fd7e89747e21f9e61ae534bfc88c53
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:07 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "kusto_databases" {
-  description = "All kusto_database resources"
-  value       = azurerm_kusto_database.kusto_databases
-}
 output "kusto_databases_cluster_name" {
-  description = "List of cluster_name values across all kusto_databases"
-  value       = [for k, v in azurerm_kusto_database.kusto_databases : v.cluster_name]
+  description = "Map of cluster_name values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.cluster_name }
 }
 output "kusto_databases_hot_cache_period" {
-  description = "List of hot_cache_period values across all kusto_databases"
-  value       = [for k, v in azurerm_kusto_database.kusto_databases : v.hot_cache_period]
+  description = "Map of hot_cache_period values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.hot_cache_period }
 }
 output "kusto_databases_location" {
-  description = "List of location values across all kusto_databases"
-  value       = [for k, v in azurerm_kusto_database.kusto_databases : v.location]
+  description = "Map of location values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.location }
 }
 output "kusto_databases_name" {
-  description = "List of name values across all kusto_databases"
-  value       = [for k, v in azurerm_kusto_database.kusto_databases : v.name]
+  description = "Map of name values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.name }
 }
 output "kusto_databases_resource_group_name" {
-  description = "List of resource_group_name values across all kusto_databases"
-  value       = [for k, v in azurerm_kusto_database.kusto_databases : v.resource_group_name]
+  description = "Map of resource_group_name values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.resource_group_name }
 }
 output "kusto_databases_size" {
-  description = "List of size values across all kusto_databases"
-  value       = [for k, v in azurerm_kusto_database.kusto_databases : v.size]
+  description = "Map of size values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.size }
 }
 output "kusto_databases_soft_delete_period" {
-  description = "List of soft_delete_period values across all kusto_databases"
-  value       = [for k, v in azurerm_kusto_database.kusto_databases : v.soft_delete_period]
+  description = "Map of soft_delete_period values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.soft_delete_period }
 }
 
