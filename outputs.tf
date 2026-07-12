7693f9076b0d284720c5fb2b9c0f584c561be06e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "kusto_databases_id" {
+  description = "Map of id values across all kusto_databases, keyed the same as var.kusto_databases"
+  value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.id }
+}
 output "kusto_databases_cluster_name" {
   description = "Map of cluster_name values across all kusto_databases, keyed the same as var.kusto_databases"
   value       = { for k, v in azurerm_kusto_database.kusto_databases : k => v.cluster_name }
